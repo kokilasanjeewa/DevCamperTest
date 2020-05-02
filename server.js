b2138010de514241220dcca86a3163bf36f23a2a
kokilasanjeewa <45851809+kokilasanjeewa@users.noreply.github.com>
2020-05-02 17:34:09 +0530

    update 2

--- server.js
+++ server.js
@@ -29,3 +29,5 @@ const port = 5000;
 server.listen(port, '127.0.0.1', () => {
   console.log(`Server running on port ${port}`);
 });
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+// 1.xx information 2.xx 200 success 201 created 202 no content 4.xx 400 bad 401 unauthorize 403 Forbidden 404 not found 500 Internal Server Error 501 Not Implemented 302 Found 300 Multiple Choice 301 Moved Permanently

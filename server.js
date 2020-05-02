44e62b00f737dbd8fc90c2720d5645a69161e9e4
kokilasanjeewa <45851809+kokilasanjeewa@users.noreply.github.com>
2020-05-02 17:45:41 +0530

    update 3

--- server.js
+++ server.js
@@ -13,13 +13,19 @@ const server = http.createServer((req, res) => {
   const { headers, url, method } = req;
   console.log(headers, url);
   //res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('X-Powerd', 'Node');
+  res.statusCode = 404;
+  // setHeader == writeHead
+  //res.setHeader('Content-Type', 'application/json');
+  //res.setHeader('X-Powerd', 'Node');
+  res.writeHead(404, {
+    'Content-Type': 'application/json',
+    'X-Powerd': 'Node',
+  });
   res.write('<h1>hello</h1>');
   res.end(
     JSON.stringify({
-      success: true,
-      data: todo,
+      success: false,
+      data: null,
       error: 'error on todo',
     }),
   );
@@ -30,4 +36,4 @@ server.listen(port, '127.0.0.1', () => {
   console.log(`Server running on port ${port}`);
 });
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-// 1.xx information 2.xx 200 success 201 created 202 no content 4.xx 400 bad 401 unauthorize 403 Forbidden 404 not found 500 Internal Server Error 501 Not Implemented 302 Found 300 Multiple Choice 301 Moved Permanently
+// 1.xx information 2.xx 200 success 201 created 202 no content 4.xx 400 bad 401 unauthorize 403 Forbidden 404 not found 500 Internal Server Error 501 Not Implemented 302 Found 300 Multiple Choice 301 Moved Permanently 304 Not Modified
